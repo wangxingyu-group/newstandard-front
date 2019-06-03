9647bc706de0c14a217426f2b2881deca7fb6ec8
wangzhenjie1101 <wzj941101>
2019-06-03 13:45:40 +0800

    节假日2019-06-03

---
+++ src/views/system/base/workdays/main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <calendar />
+  </div>
+</template>
+<script>
+import calendar from './calendar'
+export default {
+  components: { calendar },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
