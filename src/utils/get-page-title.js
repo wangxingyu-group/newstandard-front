48340c673db17bbd4d57402023f8a88fbc07b1e3
WangXingYu-Eric <wxy330886959@gmail.com>
2019-06-06 14:21:09 +0800

    首页曲线图时间切换按钮

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || '中科软智能联络中心'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
